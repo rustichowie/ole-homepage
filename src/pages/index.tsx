50130bfb77afc9b4ba77fc6ccff4925dd7e59c6c
Håvard Finvåg Olsen <havard.olsen@knowit.no>
2023-12-06 21:12:04 +0100

    fixed undefined issues

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -61,17 +61,17 @@ export default function IndexPage(
         {images && <Hero image={images[0]} />}
       </section>
       <article className="">
-        <section className="mb-8 bg-base-200 py-8 text-center">
-          <h2 className="mb-8 text-3xl text-center">Våre tjenester</h2>
-          <div
-            className={`m-auto max-w-screen-xl w-full grid gap-4 ${
-              highlightedServices.length <= 3
-                ? 'grid-cols-1 sm:grid-cols-3'
-                : 'grid-cols-1 sm:grid-cols-3 md:grid-cols-4'
-            }`}
-          >
-            {highlightedServices &&
-              highlightedServices.map((service, index) => {
+        {highlightedServices && (
+          <section className="mb-8 bg-base-200 py-8 text-center">
+            <h2 className="mb-8 text-3xl text-center">Våre tjenester</h2>
+            <div
+              className={`m-auto max-w-screen-xl w-full grid gap-4 ${
+                highlightedServices.length <= 3
+                  ? 'grid-cols-1 sm:grid-cols-3'
+                  : 'grid-cols-1 sm:grid-cols-3 md:grid-cols-4'
+              }`}
+            >
+              {highlightedServices.map((service, index) => {
                 return (
                   <Card
                     description={service.excerpt}
@@ -80,30 +80,33 @@ export default function IndexPage(
                   />
                 )
               })}
-          </div>
-        </section>
-        <section className="mx-auto max-w-screen-xl w-full mb-8">
-          <h2 className="mb-4 text-3xl text-center">Tidligere prosjekter</h2>
-          <div className="grid gap-4 lg:grid-cols-1 sm:grid-cols-2 xs:grid-cols-1">
-            {highlightedJobs &&
-              highlightedJobs.map((job, index) => {
-                return (
-                  <div
-                    key={'highlightedService-' + index}
-                    className="xs:mb-4 lg:mb-8 flex"
-                  >
-                    <HighlightedSection
-                      color={index % 2 == 0 ? 'primary' : 'secondary'}
-                      image={job.mainImage}
-                      imagePlacement={index % 2 == 0 ? 'left' : 'right'}
-                      title={job.title}
-                      description={job.excerpt}
-                    />
-                  </div>
-                )
-              })}
-          </div>
-        </section>
+            </div>
+          </section>
+        )}
+        {highlightedJobs && (
+          <section className="mx-auto max-w-screen-xl w-full mb-8">
+            <h2 className="mb-4 text-3xl text-center">Tidligere prosjekter</h2>
+            <div className="grid gap-4 lg:grid-cols-1 sm:grid-cols-2 xs:grid-cols-1">
+              {highlightedJobs &&
+                highlightedJobs.map((job, index) => {
+                  return (
+                    <div
+                      key={'highlightedService-' + index}
+                      className="xs:mb-4 lg:mb-8 flex"
+                    >
+                      <HighlightedSection
+                        color={index % 2 == 0 ? 'primary' : 'secondary'}
+                        image={job.mainImage}
+                        imagePlacement={index % 2 == 0 ? 'left' : 'right'}
+                        title={job.title}
+                        description={job.excerpt}
+                      />
+                    </div>
+                  )
+                })}
+            </div>
+          </section>
+        )}
       </article>
       <footer className="footer p-10 bg-base-200 text-base-content">
         <aside>
