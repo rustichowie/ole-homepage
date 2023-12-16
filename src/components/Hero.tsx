333e286368a9a100d2e78c13e502f37a7ac06af3
Håvard Finvåg Olsen <havard.olsen@knowit.no>
2023-12-16 21:33:09 +0100

    fixed build error

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image'
 import React, { useState } from 'react'
 import useDimensions from 'react-cool-dimensions'
 import { Image as SanityImage } from 'sanity'
+
 import { urlForImage } from '~/lib/sanity.image'
 
 export interface HeroProps {
