1f4ae46bb94407b69da010a48b93ac37db93d2ac
Håvard Finvåg Olsen <havard.olsen@knowit.no>
2023-12-22 22:11:49 +0100

    Added google verification

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,10 @@ export default function Document() {
           src="https://kit.fontawesome.com/64c000d950.js"
           crossOrigin="anonymous"
         ></Script>
+        <meta
+          name="google-site-verification"
+          content="4kebsVUgR3UFUXa6Jcu-yUblhdo36uBtzUOSrbwBrT0"
+        />
       </Head>
       <body>
         <Main />
